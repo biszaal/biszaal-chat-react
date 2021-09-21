3629b8091ca865d883f826a49a69debc1e6b7514
biszaal <biszaal@yahoo.com>
2021-09-21 20:00:50 +0545

    second update

--- src/firebase.js
+++ src/firebase.js
@@ -3,11 +3,11 @@ import "firebase/auth";
 
 export const auth = firebase
   .initializeApp({
-    apiKey: "AIzaSyBW1PUWQtZhZmEx8DWGVJjAgwQhi3K1_Tw",
-    authDomain: "auth-demo-c1925.firebaseapp.com",
-    projectId: "auth-demo-c1925",
-    storageBucket: "auth-demo-c1925.appspot.com",
-    messagingSenderId: "746557330045",
-    appId: "1:746557330045:web:059113e6cc70c0bb8d1714",
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
   })
   .auth();
